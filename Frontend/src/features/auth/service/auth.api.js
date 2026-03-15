d465f827b90d62ebf059cde4032bac91b19753d9
Prerna Sharma <prernasharma0925@gmail.com>
2026-03-16 02:23:29 +0530

    feat(auth): implement authentication with Redux, add login and register pages

--- Frontend/src/features/auth/service/auth.api.js
+++ Frontend/src/features/auth/service/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const api = axios.create({
     baseURL: "http://localhost:3000",   
-     withCredentials: true,
+    withCredentials: true,
 })
 
 export async function register({email, password, username}) {
@@ -14,5 +14,10 @@ export async function register({email, password, username}) {
 
 export async function login({ email, password }) {
     const response = await api.post("/api/auth/login", { email, password })
+    return response.data
+}
+
+export async function getMe() {
+    const response = await api.get("/api/auth/me")
     return response.data
 }
